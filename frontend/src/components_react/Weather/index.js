3011595ce68cf044776667fb2c5a7646843f2b54
dknight <knight1seven@gmail.com>
2021-05-12 11:27:43 -0400

    latest update geolocation

--- frontend/src/components_react/Weather/index.js
+++ frontend/src/components_react/Weather/index.js
@@ -5,19 +5,57 @@ import { AuthUserContext, withAuthorization } from '../Session';
 
 function WeatherPage() {
     const[weather, setWeather] = useState([]);
-    const[lat, setLat] = useState("")
-    const[lon, setLon] = useState("")
-    const[time, setTime] = useState("")
+    const[userLocation, setUserLocation] = useState([]);
+    const[currentLocation, setCurrentLocation] = useState("");
 
 
     const userId = useContext(FirebaseContext).auth.currentUser.uid 
 
+    // useEffect(() => {
+    //     navigator.geolocation.getCurrentPosition((position) => {
+    //         let coordinates = position.coords;
+    //         setCurrentLocation(coordinates);
+    //         console.log(coordinates)
+    //     }, (err) => {
+    //         console.warn(`Error(${err.code}): ${err.message}`);
+    //     }, {
+    //         enableHighAccuracy: true,
+    //         timeout: 5000,
+    //         maximumAge: 0
+    //     });
+    // }, []);
+
+
+
     async function getWeather() {
+
+    // navigator.geolocation.getCurrentPosition(
+    //         function(location) { console.log(location) }
+    //     )
         // const configs = {
         //     method: 'get',
         //     data: JSON.stringify({}),
         //     headers: {"Content-Type": "application/json"}
     
+    
+    // function getPosition(location) {
+    //     return location
+    // }
+    // const location = await navigator.geolocation.getCurrentPosition(getPosition, getError)
+    // console.log(location)
+
+    const location = await fetch (navigator.geolocation.getCurrentPosition(
+        function(location) { console.log(location) }
+    ))
+
+    async function getPosition(location) {
+        return location
+    }
+
+    function getError(error) {
+        return console.log(error)
+    }
+
 
         const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=40.71&lon=-74.00&dt=20215101619&lang=en&appid=6693af864d042d45210a62d91db9b718`);
         const userWeather = await response.json();
@@ -30,6 +68,10 @@ function WeatherPage() {
 
         // return {success: false, error: response.statusText };
 
+        const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=40.71,-74.00&key=AIzaSyC98mWMfSiEOhKrhwhN04WW2dKb_2lT_QY`);
+        const userLocation = await response.json();
+        console.log(userLocation)
+        setUserLocation(userLocation);
     }
 
 
@@ -38,6 +80,7 @@ function WeatherPage() {
         return (
             <div>
                 <h2>Current Weather</h2>
+                {/* <button onClick={() => console.log(currentLocation)}>Show coordinates</button> */}
                 
             </div>
 
